d2bdb66a0b5352f52125b35174573407e7fb4d22
Abhinash <abhinash.pandey@travelkosh.com>
2020-04-15 18:20:01 +0530

    routing completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', redirectTo : '/recipes', pathMatch : 'full'},//since empty path is common for each path so
+  {path : 'recipes', component : RecipesComponent},
+  {path : 'shopping-list', component : ShoppingListComponent},     //we are checking load this if complete path is empty
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
